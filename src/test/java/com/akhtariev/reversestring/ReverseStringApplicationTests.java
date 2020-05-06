a4ba9199559fea190d702b2cd36b1ce771305ecc
Roman Akhtariev <ackhtariev@gmail.com>
2020-05-05 17:55:36 -0700

    Initialize project

---
+++ src/test/java/com/akhtariev/reversestring/ReverseStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhtariev.reversestring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
